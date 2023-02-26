30a9fad4bc2b1c46147bb6ff7a1d784415b3978f
Philemon Eniola <dev.mcenny@gmail.com>
2023-02-26 03:09:59 +0000

    layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -31,6 +31,19 @@ export default function Layout ({children}: any){
   return(
     <div>
       <TopBar showNav={showNav} setShowNav={setShowNav}/>
+      <Transition
+       as={Fragment}
+       show={showNav}
+       enter='transform transition duration-[400ms]'
+       enterFrom='translate-x-full'
+       enterTo='translate-x-0'
+       leave='transform duration-[400ms] transition ease-in-out'
+       leaveFrom='translate-x-0'
+       leaveTo='-translate-x-full'
+      >
+        <SideBar showNav={showNav}/>
+      </Transition>
+      <main className={`pt-16 transition-all duration-[400ms] ${showNav && !isMobile ? 'pl-56' : ''}`}></main>
       
       {children}
     </div>
